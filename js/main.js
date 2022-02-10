8efb243e86b32650ef095a2884a152484aa4a5c7
saidmtanzania <saidmtanzania@gmail.com>
2022-02-10 14:45:58 +0300

    final changes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function Agecalc(){
+    var getAge = document.getElementById('dAge').value;
+    getAge=parseFloat(getAge);
+    if (isNaN(getAge)||getAge<0.1){
+        alert("cant be blank! or age cant be below 0.0")
+    }else{
+    var humanAge = ((getAge-2)*4)+21;
+    document.getElementById('dAge').value="";
+    document.getElementById('result').innerHTML =`
+    Your Dog is ${humanAge} Years old in Human Years.`;
+    }
+}
