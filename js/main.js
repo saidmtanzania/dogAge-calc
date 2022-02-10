87020547825cf83da540d9819da3d30e55588ecb
saidmtanzania <saidmtanzania@gmail.com>
2022-02-10 14:55:20 +0300

    final changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function Agecalc(){
     getAge=parseFloat(getAge);
     if (isNaN(getAge)||getAge<0.1){
         alert("cant be blank! or age cant be below 0.0")
+        document.getElementById('result').innerHTML = "";
     }else{
     var humanAge = ((getAge-2)*4)+21;
     document.getElementById('dAge').value="";
